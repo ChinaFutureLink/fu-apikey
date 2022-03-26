33ff6fe96db1e3ee9f45473bcea29edcfa90c3dc
Leskur <33910980+Leskur@users.noreply.github.com>
2022-03-26 14:14:13 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const urlMd5 = require("url-md5");
+
+urlMd5.config.pathRewriter = function (pathname, raw) {
+    return (raw?.method || "get").toUpperCase() + pathname.replace("/fu/api", "");
+}
+
+module.exports = {
+    encode: function (options) {
+        return urlMd5.encode(options);
+    },
+    setSalt: function (salt) {
+        urlMd5.config.salt = salt;
+    }
+}
